95831b4af096721534558949fac6766b9893cc76
MrEhtsham0 <mr_ehtsham@yahoo.com>
2024-04-01 18:04:30 +0500

    Adding flask model inside the projecct

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 app = Flask(__name__)
 regmodel = pickle.load(open('regmodel.pkl','rb'))
+scaledmodel = pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -12,4 +13,15 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data =request.json
+    data =request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    transformed_data = scaledmodel.transform(np.array(list(data.values())).reshape(1,-1))
+    output = regmodel.predict(transformed_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
