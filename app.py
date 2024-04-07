84213f078976bf37ad848ebf1cadcb577a48f97a
MrEhtsham0 <mr_ehtsham@yahoo.com>
2024-04-07 15:47:59 +0500

    Commiting html file and python code

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ scaledmodel = pickle.load(open('scaling.pkl','rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
     data =request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
@@ -21,7 +21,13 @@ def predict():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_ouput = scaledmodel.transform(np.array(data).reshape(1,-1))
+    output = regmodel.predict(final_ouput)[0 ]
+    return render_template('index.html',predicted_price="The house price is {}").format(output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
